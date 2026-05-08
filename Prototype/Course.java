17f581a6e07c07dd633b49d1039da66b074a3753
JeongJunYoung <jjyoung05220522@gmail.com>
2026-05-09 00:14:40 +0900

    과목관련 메인메서드 MyApp으로 이동 과목속성에 학점, 평가항목 추가 기존프로토타입 주석처리

--- Prototype/Course.java
+++ Prototype/Course.java
@@ -5,40 +5,21 @@ import java.util.Scanner;
  * Course 과목 객체를 만드는 클래스
  *
  * @author (작성자 이름)
- * @version 2026-05-13
+ * @version 2026-05-08
  */
 public class Course {
-    private String courseTitle;
+    private String courseTitle;     //과목명
+    private int courseCredit;       //학점
+    private String evaluationItem;  //평가항목(Ex:시험, 과제, 출석)
 
     public Course(String courseTitle) {
         this.courseTitle = courseTitle;
+        this.courseCredit = courseCredit;
+        this.evaluationItem = evaluationItem;
     }
 
     public String getCourseTitle() {
         return courseTitle;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("입력할 과목의 개수를 정수로 입력하세요: ");
-        int count = scanner.nextInt();
-        scanner.nextLine(); 
-
-        Course[] courseArray = new Course[count];
-
-        for (int i = 0; i < count; i++) {
-            System.out.print((i + 1) + "번째 과목명: ");
-            String inputTitle = scanner.nextLine();
-            
-            courseArray[i] = new Course(inputTitle);
-        }
-
-        System.out.println("\n--- 저장된 과목 목록 ---");
-        for (Course c : courseArray) {
-            System.out.println("과목명: " + c.getCourseTitle());
-        }
-
-        scanner.close();
-    }
 }
