a3d6406bc334381595a313c7c82fcdd291af1752
JeongJunYoung <jjyoung05220522@gmail.com>
2026-05-13 04:13:35 +0900

    과목별 학점 계산 기능 추가

--- Prototype/MyApp.java
+++ Prototype/MyApp.java
@@ -102,7 +102,7 @@ public class MyApp
     }
 
     // 학생 정보 입력
-    public static int addStudent(Scanner scanner,Student[] studentDB, Course[] courseDB, int totalStudent, int totalCourse) {
+    public static int addStudent(Scanner scanner,Student[] studentDB, Course[] courseDB, int totalStudent, int totalCourse) {      
         System.out.print("학생 이름>> ");
         String studentName = scanner.next();
         System.out.print("학생 학번>> ");
@@ -133,6 +133,7 @@ public class MyApp
 
         for (int i = 0 ; i < courseCount ; i++) {
             String inputCourse;     //과목이 데이터베이스에 입력되어 있는지 확인하기 위해 입력값을 저장해둘 변수
+            System.out.println();
             System.out.print("수강과목명 입력>> ");
             inputCourse = scanner.next();
 
@@ -201,30 +202,136 @@ public class MyApp
                 System.out.println("이름: " + student.getStudentName());
                 System.out.println("학번: " + student.getStudentNum());
 
-                for (int i = 0; i < student.getCourseStudent().length; i++) {   //수강중인 과목개수만큼 반복
+                for (int i = 0; i < student.getCourseStudent().length; i++) {   //수강중인 과목 개수만큼 반복
                     String courseName = student.getCourseStudent()[i];    //현재 출력중인 과목명
                     System.out.println("수강과목: " + courseName);
-                    int[] courseScores = student.getScores()[i];    //현재 출력중인 과목의 평가항목별 점수 불러오기
-
+                    int[] courseScores = student.getScores()[i];    //검색학생 점수 불러오기
+                    int scoreSum = 0;
                     //해당 과목의 평가항목별 점수 출력
                     for (int k = 0; k < totalCourse; k++) {
                         if (courseDB[k].getCourseName().equals(courseName)) { //과목DB에서 출력중인 과목 데이터 찾기
-                            String[][] items = courseDB[k].getEvaluationItem();
+                            String[][] items = courseDB[k].getEvaluationItem(); //출력중인 과목의 평가항목 불러오기
 
                             for (int j = 0; j < courseScores.length; j++) {
-
-                                System.out.println(items[j][0] + " 점수 : " + courseScores[j]);
+                                System.out.print(items[j][0] + " 점수 : " + courseScores[j]);
+                                scoreSum += courseScores[j];
                             }
                         }
                     }
+
+                    //해당과목 총점 출력
+                    System.out.println(courseName + "과목 총 점수는 : " + scoreSum + "입니다.");
                 }
             }
         }
 
         //학생 DB에 해당 학생 없을경우 문구 출력
         if (!found) {
-            System.out.println();
+            System.out.println("");;
             System.out.println("해당 학번의 학생을 찾을 수 없습니다.");
+        }
+    }
+
+    //과목의 평가항목 총점 계산 메서드
+    public static int courseTotalScoreCalculate(Student student , String courseName){
+        String[] courses = student.getCourseStudent() ; //총점 계산할 학생의 수강과목 불러오기
+        int[][] scores = student.getScores();   //점수 불러오기
+        //
+        for (int i = 0; i < courses.length; i++) {
+            if (courses[i].equals(courseName)) {    //불러온 수강과목중 해당 과목만 뽑아내기
+                int sum = 0;
+                for (int j = 0 ; j < scores[i].length ; j++) {  //받은 평가항목 점수의 총합 구하기
+                    sum += scores[i][j];
+                }
+                return sum;
+            }
+        }
+        return -1;
+    }
+
+    //과목별 성적 계산
+    public static void courseCalculate(Scanner scanner , Student[] studentsDB, int totalStudent){
+        System.out.print("성적조회할 과목명을 입력하세요 >> ");
+        String findCourse = scanner.next(); //조회할 과목명
+        Student[] rankStudents = new Student[totalStudent]; //수강학생 점수 순위를 넣을 배열
+        int[] totals = new int[totalStudent];   //
+        int count = 0;
+
+        // 해당 과목 수강 학생 저장
+        for (int i = 0; i < totalStudent; i++) {
+            Student student = studentsDB[i];    //학생DB에서 수강자만 뽑아오기
+            int total = courseTotalScoreCalculate(student, findCourse); //순위 계산을 위해 해당 학생의 총점을 따로 저장
+
+            if (total != -1) {
+                rankStudents[count] = student;  //수강학생정보가 들어있는 데이터베이스(순위 계산을 위해 totlas와 같은 인덱스로 학생객체 참조)
+                totals[count] = total;  //총점이 입력된 데이터베이스 (순위 계산을 위해 rankStudents와 같은 인덱스 사용)
+                count++;    //수강자 구분 인덱스
+            }
+        }
+
+        // 총점 기준 내림차순 정렬
+        for (int i = 0; i < count - 1; i++) {
+            for (int j = i + 1; j < count; j++) {
+                if (totals[i] < totals[j]) {    //앞 사람과 뒷사람 총점 비교하며 순위 정렬 Ex)만약 이후에 입력된 사람이 더 높다면 서로 순위 바꾸기
+                    int tempScore = totals[i];  
+                    totals[i] = totals[j];
+                    totals[j] = tempScore;
+                    Student tempStudent = rankStudents[i];
+                    rankStudents[i] = rankStudents[j];
+                    rankStudents[j] = tempStudent;
+                }
+            }
+        }
+        System.out.println("");;
+        
+        //학점 나누기 전 순위표 출력
+        System.out.println("[" + findCourse + " 순위표]");
+        for (int i = 0; i < count; i++) {
+            System.out.println((i + 1) + "등 " + rankStudents[i].getStudentName() + " " + rankStudents[i].getStudentNum() + " " + totals[i] + "점");
+        }
+        
+        //등급 기준 입력
+        System.out.print("A 등급 최소 점수 >> ");
+        int aCut = scanner.nextInt();
+        System.out.print("B 등급 최소 점수 >> ");
+        int bCut = scanner.nextInt();
+        System.out.print("C 등급 최소 점수 >> ");
+        int cCut = scanner.nextInt();
+
+        //선문대학교 교칙에 따라 35%만 A 또는 A+ 가능
+        int aLimit = (int)Math.ceil(count * 0.35);
+        //선문대학교 교칙에 따라 70%만 B 또는 B+ 가능
+        int bLimit = (int)Math.ceil(count * 0.70);
+
+        // 최종 순위 출력
+        System.out.println("");;
+        System.out.println("[" + findCourse + " 성적 순위]");
+
+        System.out.println("");;
+        System.out.println("[" + findCourse + " 최종 성적]");
+        
+        //학생들에게 기준에 맞추어 학점 부여
+        for (int i = 0; i < count; i++) {
+            String grade;
+            if (i < aLimit
+            && totals[i] >= aCut) {
+                grade = "A";
+            }
+
+            else if (i < bLimit
+            && totals[i] >= bCut) {
+                grade = "B";
+            }
+
+            else if (totals[i] >= cCut) {
+                grade = "C";
+            }
+
+            else {
+                grade = "F";
+            }
+
+            System.out.println((i + 1) + "등 " + rankStudents[i].getStudentName() + " " + rankStudents[i].getStudentNum() + " " + totals[i] + "점 " + grade);
         }
     }
 
@@ -367,9 +474,11 @@ public class MyApp
                                 studentMenu = 0;
                         }
                     }
+                    studentMenu = 0;
+                    mainMenu = 0;
                     break;
 
-                    성적처리 메뉴
+                    //성적처리 메뉴
                 case 3:
                     while(creditMenu != 3){
                         mainMenu = 0;
@@ -394,15 +503,16 @@ public class MyApp
 
                                 //----------------------------------------과목별 성적조회 탭
                             case 1:
-                                studentCredit(scanner);       
+                                courseCalculate(scanner , studentDB , totalStudent);
                                 creditMenu = 0;
                                 break;
 
                                 //--------------------------------------------학생별 성적조회 탭
-                            case 2:
-                                courseCreditCalculate(scanner);
-                                creditMenu = 0;
-                                break;
+                                // case 2:
+                                // studentScoreCalculate(scanner , studentDB , courseDB , totalStudent , totalCourse);
+                                // creditMenu = 0;
+                                // break;
+                                //내일 여기서부터 하자잉 ㅇㅇ 거의 다 왔다 수고했어
 
                             case 3:
                                 mainMenu = 0;
@@ -415,6 +525,8 @@ public class MyApp
                                 studentMenu = 0;
                         }
                     }
+                    creditMenu = 0;
+                    mainMenu = 0;
                     break;
 
                     //프로그램 종료
